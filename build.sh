872f3e9666c9694e99b603c1a13da3fc7a61d951
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-05-14 23:19:22 -0400

    fix problem with build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -oue pipefail
-mkdir -p ../{BUILD,BUILDROOT,RPMS,SOURCES,SRPMS}
+mkdir -p ./{BUILD,BUILDROOT,RPMS,SOURCES,SRPMS}
 wget https://github.com/sihawken/i915-sriov-dkms/archive/refs/tags/v6.1.11.tar.gz -O ./SOURCES/v6.1.11.tar.gz
 rpmbuild --define "_topdir `pwd`" -ba SPECS/i915-sriov-dkms.spec
