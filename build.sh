3232335cc612c7f59146e9a4f02f9e3d54cb462f
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-05-14 21:32:42 -0400

    change name of spec file

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir BUILDROOT
 mkdir RPMS
 mkdir SOURCES
 mkdir SRPMS
-rpmbuild --undefine=_disable_source_fetch -ba SPECS/i915_sriov_dkms.spec
+rpmbuild --undefine=_disable_source_fetch -ba SPECS/i915-sriov-dkms.spec
